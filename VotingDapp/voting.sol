d1ec238c603fb063378a21e9deb5994a9be6eca8
diero42 <69011567+diero42@users.noreply.github.com>
2021-06-17 13:42:23 -0700

    Update voting.sol
    Added functions to begin registration and election periods. Fixed bugs.

--- VotingDapp/voting.sol
+++ VotingDapp/voting.sol
@@ -18,22 +18,53 @@ contract Voting is Ownable {
   }
   
   Candidate[] public candidates;
+  bool registration = false;
+  bool election = false;
+  uint timer;
   
-  /// @notice Public function to nominate any candidate using their address.
-  /// @dev Tests if candidate has already been nominated before proceeding.
+  /// @notice Public function to nominate any user during the registration period using their address.
+  /// @dev Tests if user has already been nominated before proceeding.
   /// @param candAddress Address of the nominee.
   /// @param candName Real name of the nominee.
-  function nominateCandidate(address candAddress, string candName) public {
-    require(isCandidate[candAddress] == false);
-    candIndex[candAddress] = candidates.push(Candidate(candAddress,candName,0)) - 1;
-    isCandidate[candAddress] = true;
+  function nominateCandidate(address nomAddress, string nomName) public {
+    require(registration == true);
+    require(isCandidate[nomAddress] == false);
+    candIndex[nomAddress] = candidates.push(Candidate(nomAddress,nomName,0)) - 1;
+    isCandidate[nomAddress] = true;
   }
   
-  /// @notice Voting function. Each address can vote once for an existing candidate.
+  /// @notice Voting function. Each user can vote once for an existing candidate during the election period.
   /// @param cand Address of existing candidate.
   function vote(address cand) public {
+    require(registration == true);
     require(hasVoted[msg.sender] == false);
     require(isCandidate[cand] == true);
     candidates[candIndex[cand]].votes++;
+    hasVoted[msg.sender] == true;
+  }
+  
+  /// @notice Begins registration period. During this time,
+  /// users can nominate any user once using nominateCandidate().
+  /// @dev Will only run if it is not currently an election or registration period.
+  function beginRegistration() public onlyOwner {
+    require(registration == false);
+    require(election == false);
+    registration = true;    
+  }
+  
+  /// @notice Begins election period. During this time,
+  /// users can vote for any candidate once using nominateCandidate().
+  /// @dev Will only run if it is not currently an election or registration period.
+  function beginVoting() public onlyOwner {
+    require(registration == true);
+    require(election == false);
+    registration = false;
+    election = true;
+  }
+  
+  function endElection() public onlyOwner {
+    require(registration == false);
+    require(election == true);
+    election = false;
   }
 }
